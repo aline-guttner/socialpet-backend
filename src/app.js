6d0c1c51552bcbe50e7741109042855a32633daa
aline-guttner <aline.guttner@gmail.com>
2022-09-27 12:58:33 -0300

    commit on 'master'

--- src/app.js
+++ src/app.js
@@ -12,13 +12,14 @@ const app = express();
 
 app.options('*', cors()) // include before other routes
 
+app.use(cors());
+
 app.use((req, res, next) => {
     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
     //Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    app.use(cors());
     next();
 });
 
