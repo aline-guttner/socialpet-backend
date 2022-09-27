5b0509414d9e0180aed520ccd97f674b67f13302
aline-guttner <aline.guttner@gmail.com>
2022-09-27 13:06:05 -0300

    commit on 'master'

--- src/app.js
+++ src/app.js
@@ -10,22 +10,22 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.options('*', cors()) // include before other routes
 
 
-// app.use((req, res, next) => {
-//     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-//     res.header("Access-Control-Allow-Origin", "*");
-//     //Quais são os métodos que a conexão pode realizar na API
-//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//     app.use(cors());
-//     next();
-// });
+app.use((req, res, next) => {
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    //Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    app.use(cors());
+    next();
+});
 
-// dotenv.config();
+dotenv.config();
 
 database.on('error', (error) => console.error(error)); //alterado
 database.on('open', () => console.log('Connected to database')); //alterado
