345f4cd80dc77bf9d65b6cbc46c26a5be2c6aea5
aline-guttner <aline.guttner@gmail.com>
2022-09-27 13:13:04 -0300

    commit on 'master'

--- src/app.js
+++ src/app.js
@@ -12,15 +12,13 @@ const app = express();
 
 app.options('*', cors()) // include before other routes
 
-app.use((req, res, next) => {
-    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-    //Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    app.use(cors());
-    next();
-});
+app.use(cors({
+    allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
+    exposedHeaders: ["authorization"], // you can change the headers
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false
+}));
 
 // dotenv.config();
 
