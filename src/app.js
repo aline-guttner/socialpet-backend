5449b49e44edd36c758f88a2e0b11089043ed28f
aline-guttner <aline.guttner@gmail.com>
2022-09-27 13:55:41 -0300

    commit on 'master'

--- src/app.js
+++ src/app.js
@@ -10,16 +10,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use((req, res, next) => {
-    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    //Quais são os métodos que a conexão pode realizar na API
-    res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    app.use(cors());
-    next();
-});
+app.use(cors());
 
 require("dotenv").config();
 
