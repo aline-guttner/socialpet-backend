06e598fa0a00407841201b70f02e788cb2f2c47a
aline-guttner <aline.guttner@gmail.com>
2022-10-01 21:02:45 -0300

    commit on 'master'

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@
 import mongoose from "mongoose"
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialpet',
+    process.env.MONGODB_URI ||
+    'mongodb://localhost:27017/socialpet',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
